7ffc7f5307396b1b163fd8f2b5fa58733a11eb8f
Christopher Lim <lim@Christophers-MacBook-Pro.local>
2018-10-16 06:43:08 -0700

    adds toaster.rb

---
+++ spec/toaster.rb
@@ -0,0 +1,13 @@
+Toaster = Struct.new(:serial) do
+    def self.find_by_serial(serial)
+        new(serial)
+    end
+end
+
+    def current_toaster
+        @current_toaster ||= Toaster.find_by_serial('HHGG42')
+    end
+
+current_toaster
+
+@current_toaster = nil || Toaster.find_by_serial('HHGG42')
